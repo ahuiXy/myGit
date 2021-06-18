ce1a088448ad94a0875aef6427cf1ee67bc7eba5
xyh_glb <xuyh@qq.com>
2021-06-18 11:51:07 +0800

    fix 3

--- helloGit/src/main/java/com/xyh/HelloGit.java
+++ helloGit/src/main/java/com/xyh/HelloGit.java
@@ -3,7 +3,7 @@ package com.xyh;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("更新1");
-        String s = "gengxin 3";
+        String s = "4324234";
         System.out.println("hot fix 1");
     }
 }
