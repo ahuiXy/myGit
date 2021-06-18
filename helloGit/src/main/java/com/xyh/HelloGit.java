23166a8420a45dea1a6c5c4d731d1d9c48a67ac0
xyh_glb <xuyh@qq.com>
2021-06-18 11:44:33 +0800

    修复2

--- helloGit/src/main/java/com/xyh/HelloGit.java
+++ helloGit/src/main/java/com/xyh/HelloGit.java
@@ -3,7 +3,7 @@ package com.xyh;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("更新1");
-        System.out.println("更新2");
+        String s = "gengxin 3";
         System.out.println("hot fix 1");
     }
 }
